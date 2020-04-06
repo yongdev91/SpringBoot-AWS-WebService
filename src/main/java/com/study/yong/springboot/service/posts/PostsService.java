e530fb68eb15af27c542bf5a57e14ba786ca079e
yong <yongdev91@gmail.com>
2020-04-07 01:23:35 +0900

    4장 머스테치 학습

--- src/main/java/com/study/yong/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/yong/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.study.yong.springboot.service.posts;
 
 import com.study.yong.springboot.domain.posts.Posts;
 import com.study.yong.springboot.domain.posts.PostsRepository;
+import com.study.yong.springboot.web.dto.PostsListResponseDto;
 import com.study.yong.springboot.web.dto.PostsResponseDto;
 import com.study.yong.springboot.web.dto.PostsSaveRequestDto;
 import com.study.yong.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -27,8 +31,24 @@ public class PostsService {
         posts.update(requetDto.getTitle(), requetDto.getContent());
         return id;
     }
+
+    @Transactional
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨둠으로 조회속도가 개선됨
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+        postsRepository.delete(posts);
     }
 }
