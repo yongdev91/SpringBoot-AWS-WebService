200f8a0444cccc2f0c4e33303b1a60ac01a1c702
yong <yongdev91@gmail.com>
2020-04-06 23:54:43 +0900

    4장 머스테치 학습

---
+++ src/main/java/com/study/yong/springboot/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.study.yong.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
