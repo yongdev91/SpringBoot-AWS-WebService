d157e9d2f319fcec9799c6cedbfca548d387336a
yong <yongdev91@gmail.com>
2020-04-09 01:34:10 +0900

    5장 OAuth로 로그인하기

--- src/main/java/com/study/yong/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/study/yong/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,26 +1,57 @@
 package com.study.yong.springboot.config.auth;
 
+import com.study.yong.springboot.config.auth.dto.OAuthAttributes;
+import com.study.yong.springboot.config.auth.dto.SessionUser;
+import com.study.yong.springboot.domain.user.User;
 import com.study.yong.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
+// 이 클래스는 구글 로그인 이후 가져온 사용자의 정보들을 기반으로 가입 및 정보수정, 세션 저장 등의 기능을 지원함
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
-
     private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // 너무 졸려서 여기까지만 작성하고 자야겠다.
-        return null;
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 현재 로그인 진행중인 서비스를 구분
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        //
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new
+                        SimpleGrantedAuthority(user.getRoleKey())),
+                        attributes.getAttributes(),
+                        attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail()).map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
--- src/main/java/com/study/yong/springboot/web/IndexController.java
+++ src/main/java/com/study/yong/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.study.yong.springboot.web;
 
+import com.study.yong.springboot.config.auth.dto.SessionUser;
+import com.study.yong.springboot.domain.user.User;
 import com.study.yong.springboot.service.posts.PostsService;
 import com.study.yong.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
