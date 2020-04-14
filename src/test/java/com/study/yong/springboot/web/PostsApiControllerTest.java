369afeccd491dbfba162e197b74a06391cd5dc2e
yong <yongdev91@gmail.com>
2020-04-15 08:51:04 +0900

    빌드 오류수정

--- src/test/java/com/study/yong/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/study/yong/springboot/web/PostsApiControllerTest.java
@@ -1,30 +1,26 @@
 package com.study.yong.springboot.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.yong.springboot.domain.posts.Posts;
 import com.study.yong.springboot.domain.posts.PostsRepository;
 import com.study.yong.springboot.web.dto.PostsSaveRequestDto;
 import com.study.yong.springboot.web.dto.PostsUpdateRequestDto;
-import javafx.geometry.Pos;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
