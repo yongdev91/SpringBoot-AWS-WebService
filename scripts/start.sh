bae3ac9357b093621352b68eee3a6fbb820b7585
yong <yongdev91@gmail.com>
2020-06-02 08:26:06 +0900

    jar파일 실행 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,7 +26,7 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_JAR_NAME를 profile=$IDLE_PROFILE" 로 실행합니다.
-nohup jav -jar \
+nohup java -jar \
         -Dspring.config.location=clsspath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2/user/app/application-real-db.properties \
         -Dspring.profiles.active=$IDLE_PROFILE  \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
